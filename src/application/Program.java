6a7989ac7f0f25ac7647c8e4b4230ff108c3cfd0
TarleyRodrigues <tarley.divino@gmail.com>
2022-04-08 17:39:08 -0300

    Lógica para voltar alterações feitas caso algum erro ocorra durante - Rowback/Commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class Program {
 
@@ -15,18 +16,37 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 			
+			//Para não confirmar as operações no automático:
+			conn.setAutoCommit(false);
+			
 			st = conn.createStatement();
 			
 			//saber o número de linhas afetadas
-			//Todo vendedor que pertencer ao Department 1, o salário será atualizado para 2090:
-			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
-			//Todo vendedor que pertencer ao Department 2, o salário será atualizado para 3090:
-			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			//Todo vendedor que pertencer ao Department 1, o salário será atualizado para 1999:
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 1999 WHERE DepartmentId = 1");
+			
+			int quebrar = 1;
+			if(quebrar < 2) {
+				throw new SQLException("Fake error");
+			}
+			
+			//Todo vendedor que pertencer ao Department 2, o salário será atualizado para 2550:
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 2550 WHERE DepartmentId = 2");
 
+			conn.commit();//Para confirmar que agora sim minha transação terminou
+			
 			System.out.println("rows1 " + rows1);
 			System.out.println("rows2 " + rows2);
 		}catch(SQLException e) {
-			e.printStackTrace();
+			//lógica para voltar a transação caso ela se interrompa no meio:
+			//não mudará nada no banco caso ocorra!
+			try {
+				conn.rollback();
+				throw new DbException("Transação não concluida! Caused by: " + e.getMessage());
+			} catch (SQLException e1) {
+				//Caso Rollback quebre:
+				throw new DbException("Error ao tentar voltar a transação! Caused by: " + e1.getMessage());
+			}
 		}
 		finally {
 			DB.closeStatement(st);
